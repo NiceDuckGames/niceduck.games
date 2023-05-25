dffee1bf832e793eda62300f32d0f81d2f811656
Kirby Sabol <corysabol@users.noreply.github.com>
2023-05-25 17:12:30 -0400

    init commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,17 +8,25 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Nice Duck Games`,
+    siteTitleAlt: `Nice Duck Games - We make nice games and game development tools`,
+    siteHeadline: `Nice Duck Games - Hi there welcome to our site!`,
+    siteUrl: `https://niceduck.games`,
+    siteDescription: `The site for Nice Duck Games`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@niceduckgames`,
   },
   trailingSlash: `never`,
   plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-emoji`,  // <-- this line adds emoji
+        ]
+      }
+    },
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
